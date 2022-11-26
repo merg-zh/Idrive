473237e1b0e47fb0a3372a34a2c9e7e0d37778be
merg-hz <66412416+merg-zh@users.noreply.github.com>
2022-11-26 21:48:20 +0900

    Add files via upload

--- idrive/settings.py
+++ idrive/settings.py
@@ -116,9 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = BASE_DIR + "static/"
+import os
 
-STATIC_ROOT  = BASE_DIR + "static/"
+STATIC_URL = os.path.join(BASE_DIR, "static")
+
+STATIC_ROOT  = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
