69972469474194dcd99d579f5dff0d2d951fd909
merg-hz <66412416+merg-zh@users.noreply.github.com>
2022-12-09 20:01:14 +0900

    Add files via upload

--- app_first/urls.py
+++ app_first/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.TopView, name='top'),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path('home/', views.HomeView, name="home"),
+    path('download/', views.Dy, name="download"),
     path("ajax-list/",views.Ajax_Append_List, name="ajax-list"),
     path("ajax-left/",views.Ajax_Left_List, name="ajax-left"),
 ]
